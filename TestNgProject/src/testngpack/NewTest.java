c189cd2f0a9d7751a80fdf1fb73980d8300ca6f0
priyanka <priyankajadhav3112001@gmail.com>
2024-11-14 19:25:35 +0530

    annotation,setup

---
+++ TestNgProject/src/testngpack/NewTest.java
@@ -0,0 +1,64 @@
+package testngpack;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class NewTest {
+  @Test
+  public void first() {
+	 
+	  System.out.println("I am first");
+  }
+  @Test
+  public void second() {
+	 
+	  System.out.println("I am second");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("I am beforeMethod");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("I am afterMethod");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("I am beforeClass"); 
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("I am beforeTest");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("I am beforeTest");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	System.out.println("I am afterTest");
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("I am beforeSuite");
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("I am afterSuite");
+  }
+
+}
